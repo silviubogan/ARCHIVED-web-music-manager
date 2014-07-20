291b1d78eff8ea0f03b08e2b829b230d839e310c
Ionică Bizău <bizauionica@yahoo.com>
2014-07-20 17:24:06 +0300

    Fixed #7. Replaced tabs with spaces in files written by us.

--- app.js
+++ app.js
@@ -1,93 +1,93 @@
 var Statique = require("statique"),
-	http = require("http"),
-	fs = require("fs"),
-	url = require("url"),
-        os = require("os");
+    http = require("http"),
+    fs = require("fs"),
+    url = require("url"),
+    os = require("os");
 
 var port = process.env.PORT || (os.platform() === "win32" ? 80 : 8000);
 if (process.argv[2] === "help") {
-	console.log("You can specify an argument: the filesystem path to your music collection, " +
-		"or you can run the application just with its own data, its collection of online music " +
-		"(YouTube URLs etc.).");
-	process.exit();
+    console.log("You can specify an argument: the filesystem path to your music collection, " +
+        "or you can run the application just with its own data, its collection of online music " +
+        "(YouTube URLs etc.).");
+    process.exit();
 }
 var music_collection_path = process.argv[2];
 var ytDataPath = "public/yt-data.json";
 
 Statique.server({ root: __dirname + "/public" })
-	.setRoutes({
-		"/": "/index.html",
-		"/data": function (req, res) {
-			if (!music_collection_path) {
-				res.end("[]");
-				return;
-			}
+    .setRoutes({
+        "/": "/index.html",
+        "/data": function (req, res) {
+            if (!music_collection_path) {
+                res.end("[]");
+                return;
+            }
 
-			var url_parts = url.parse(req.url, true);
-			var query = url_parts.query;
-			console.dir(query);
+            var url_parts = url.parse(req.url, true);
+            var query = url_parts.query;
+            console.dir(query);
 
-			var f = [];
-			fs.readdir(music_collection_path + query.path, function(err, files) {
-				if (err) {
-					res.end(JSON.stringify(err));
-					throw(err);
-				}
-				for (var i = 0; i < files.length; i++) {
-					if (files[i] !== "." && files[i] !== "..") {
-						f.push(files[i]);
-					}
-				}
-				for (var i = 0; i < f.length; i++) {
-					if (fs.lstatSync(music_collection_path + query.path + "/" + f[i]).isDirectory()) {
-						f[i] = {
-							title: f[i],
-							folder: true,
-							lazy: true,
-							path: query.path + "/" + f[i]
-						};
-					} else {
-						f[i] = {
-							title: f[i],
-							path: query.path + "/" + f[i]
-						};
-					}
-				}
-				res.end(JSON.stringify(f));
-			});
-		},
-		"/audio": function (req, res) {
-			var url_parts = url.parse(req.url, true);
-			var query = url_parts.query;
-			var src = query.src;
+            var f = [];
+            fs.readdir(music_collection_path + query.path, function(err, files) {
+                if (err) {
+                    res.end(JSON.stringify(err));
+                    throw(err);
+                }
+                for (var i = 0; i < files.length; i++) {
+                    if (files[i] !== "." && files[i] !== "..") {
+                        f.push(files[i]);
+                    }
+                }
+                for (var i = 0; i < f.length; i++) {
+                    if (fs.lstatSync(music_collection_path + query.path + "/" + f[i]).isDirectory()) {
+                        f[i] = {
+                            title: f[i],
+                            folder: true,
+                            lazy: true,
+                            path: query.path + "/" + f[i]
+                        };
+                    } else {
+                        f[i] = {
+                            title: f[i],
+                            path: query.path + "/" + f[i]
+                        };
+                    }
+                }
+                res.end(JSON.stringify(f));
+            });
+        },
+        "/audio": function (req, res) {
+            var url_parts = url.parse(req.url, true);
+            var query = url_parts.query;
+            var src = query.src;
 
-                        Statique.serveFile(src, 200, res, req, {
-                            "content-type": "audio/mpeg"
-                        }, music_collection_path);
-		},
-		"/post-yt-data": function (req, res) {
-			var url_parts = url.parse(req.url, true);
-			var query = url_parts.query;
-			fs.writeFile(ytDataPath, query.data, function () {
-				res.end();
-			});
-		},
-		"/yt-data": function (req, res) {
-			fs.exists(ytDataPath, function (exists) {
-				if (exists) {
-					fs.readFile(ytDataPath, function (err, data) {
-						if (err) throw err;
-						res.end(data);
-					});
-				} else {
-					fs.writeFile(ytDataPath, "[]", function (err) {
-						if (err) throw err;
-						res.end("[]");
-					});
-				}
-			});
-		}
-	});
+            Statique.serveFile(src, 200, res, req, {
+                "content-type": "audio/mpeg"
+            }, music_collection_path);
+        },
+        "/post-yt-data": function (req, res) {
+            var url_parts = url.parse(req.url, true);
+            var query = url_parts.query;
+            fs.writeFile(ytDataPath, query.data, function () {
+                res.end();
+            });
+        },
+        "/yt-data": function (req, res) {
+            fs.exists(ytDataPath, function (exists) {
+                if (exists) {
+                    fs.readFile(ytDataPath, function (err, data) {
+                        if (err) throw err;
+                        res.end(data);
+                    });
+                } else {
+                    fs.writeFile(ytDataPath, "[]", function (err) {
+                        if (err) throw err;
+                        res.end("[]");
+                    });
+                }
+            });
+        }
+    });
 
 http.createServer(Statique.serve).listen(port);
 
--- public/script.js
+++ public/script.js
@@ -1,117 +1,117 @@
 var $ytIframeApiScript = $("<script>").attr("src",
-		"https://www.youtube.com/iframe_api");
+        "https://www.youtube.com/iframe_api");
 var firstScriptTag = $("script:first").get(0);
 firstScriptTag.parentNode.insertBefore($ytIframeApiScript.get(0), firstScriptTag);
 
 var player;
 
 function playYtId(id) {
-	player.loadVideoById({
-		"videoId": id,
-		"suggestedQuality": "large"
-	})
+    player.loadVideoById({
+        "videoId": id,
+        "suggestedQuality": "large"
+    })
 }
 
 function onYouTubeIframeAPIReady() {
-	player = new YT.Player('yt-player', {
-		height: '390',
-		width: '640',
-		events: {
-			'onReady': function (event) {
-				event.target.playVideo();
-			}
-		}
-	});
+    player = new YT.Player('yt-player', {
+        height: '390',
+        width: '640',
+        events: {
+            'onReady': function (event) {
+                event.target.playVideo();
+            }
+        }
+    });
 }
 
 var ytData;
 
 function postYtData(cb) {
-	var d = { data: JSON.stringify(ytData) };
-	if (cb) {
-		$.get("/post-yt-data", d, cb);
-	} else {
-		$.get("/post-yt-data", d);
-	}
+    var d = { data: JSON.stringify(ytData) };
+    if (cb) {
+        $.get("/post-yt-data", d, cb);
+    } else {
+        $.get("/post-yt-data", d);
+    }
 }
 function loadYtData(cb) {
-	$.get("/yt-data", function (data) {
-		ytData = JSON.parse(data);
-		if (cb) {
-			cb();
-		}
-	});
+    $.get("/yt-data", function (data) {
+        ytData = JSON.parse(data);
+        if (cb) {
+            cb();
+        }
+    });
 }
 
 $(function () {
-	var $tree = $("#tree"),
-	    $ytAddBtn = $("#yt-add-btn"),
-	    $ytUrl = $("#yt-url"),
-	    $ytList = $("#yt-list"),
-		$ytClearUrlBtn = $("#yt-clear-url-btn"),
-		$crtFileIndicator = $("#current-file-indicator"),
-		$audioPlayer = $("#audio");
+    var $tree = $("#tree"),
+        $ytAddBtn = $("#yt-add-btn"),
+        $ytUrl = $("#yt-url"),
+        $ytList = $("#yt-list"),
+        $ytClearUrlBtn = $("#yt-clear-url-btn"),
+        $crtFileIndicator = $("#current-file-indicator"),
+        $audioPlayer = $("#audio");
 
-	$tree.fancytree({
-		source: [
-		    { title: "/", folder: true, lazy: true, expanded: true, selected: true, path: "/" }
-		],
-		lazyLoad: function (ev, data) {
-			var node = data.node;
+    $tree.fancytree({
+        source: [
+            { title: "/", folder: true, lazy: true, expanded: true, selected: true, path: "/" }
+        ],
+        lazyLoad: function (ev, data) {
+            var node = data.node;
 
-			var d = { path: "/" };
-			if (node.data.path) {
-				d.path = node.data.path;
-			}
+            var d = { path: "/" };
+            if (node.data.path) {
+                d.path = node.data.path;
+            }
 
-			data.result = {
-				url: "/data",
-				data: d,
-				cache: false
-			};
-		},
-		activate: function (ev, data) {
-			if (!data.node.folder) {
-				$crtFileIndicator.text(data.node.title);
-				$audioPlayer.attr({
+            data.result = {
+                url: "/data",
+                data: d,
+                cache: false
+            };
+        },
+        activate: function (ev, data) {
+            if (!data.node.folder) {
+                $crtFileIndicator.text(data.node.title);
+                $audioPlayer.attr({
                                     "src": "/audio?src=" + data.node.data.path,
                                     "autoplay": ""
                                 });
-			}
-		}
-	});
-	$ytUrl.keyup(function (e) {
-		if (e.keyCode == 13) {
-			$ytAddBtn.click();
-		}
-	});
-	$ytClearUrlBtn.click(function () {
-		$ytUrl.val("");
-	});
-	function addYtId(id) {
-		var $li = $("<li>");
-		$li.append("<a href='http://www.youtube.com/watch?v=" + id + "'>" + id + "</a>");
-		var $playBtn = $("<input type='button' value='>'>");
-		$playBtn.click(function () {
-			playYtId(id);
-		});
-		$li.append($playBtn);
-		$ytList.append($li);
-	}
-	$ytAddBtn.click(function () {
-		var url = $ytUrl.val().trim();
-		if (url.length === 0) return;
-		var id = $.url(url).param("v");
-		ytData.push(id);
-		postYtData();
-		addYtId(id);
-		$ytUrl.val("");
-	});
-	loadYtData(function () {
-		$ytList.empty();
-		for (var i = 0; i < ytData.length; i++) {
-			addYtId(ytData[i]);
-		}
-	});
+            }
+        }
+    });
+    $ytUrl.keyup(function (e) {
+        if (e.keyCode == 13) {
+            $ytAddBtn.click();
+        }
+    });
+    $ytClearUrlBtn.click(function () {
+        $ytUrl.val("");
+    });
+    function addYtId(id) {
+        var $li = $("<li>");
+        $li.append("<a href='http://www.youtube.com/watch?v=" + id + "'>" + id + "</a>");
+        var $playBtn = $("<input type='button' value='>'>");
+        $playBtn.click(function () {
+            playYtId(id);
+        });
+        $li.append($playBtn);
+        $ytList.append($li);
+    }
+    $ytAddBtn.click(function () {
+        var url = $ytUrl.val().trim();
+        if (url.length === 0) return;
+        var id = $.url(url).param("v");
+        ytData.push(id);
+        postYtData();
+        addYtId(id);
+        $ytUrl.val("");
+    });
+    loadYtData(function () {
+        $ytList.empty();
+        for (var i = 0; i < ytData.length; i++) {
+            addYtId(ytData[i]);
+        }
+    });
 });
 
